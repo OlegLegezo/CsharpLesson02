2d7f93a4a851c0c007f5b6ecb4ab90cdbe7faf12
OlegLegezo <oleglegezo@yandex.ru>
2022-05-15 09:44:12 +0300

    поиск макс массива

---
+++ ex01_v3/Program.cs
@@ -0,0 +1,21 @@
+﻿// поиск максимума из 9 переменных вриант 2
+// делим игроков на три тройки, далее финал из победителей троек
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//          0   1  2  3  4  5  6  7  8
+int[]array={11,211,31,41,15,61,17,18,19};
+//array[0] = 12; //записали в ячейку номер 0 число 12
+//Console.WriteLine(array[0]);
+//int max = Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+
+
+int max = Max(Max(array[0],array[1],array[2]),Max(array[3],array[4],array[5]),Max(array[6],array[7],array[8]));
+
+Console.WriteLine(max);
+
